d3326ce827db11c94e54ca73449f96e439fbe376
Furizax <148867645+Furizax@users.noreply.github.com>
2026-04-26 23:14:36 -0400

    Adding Jump and flip (need to be fix)

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,11 +7,16 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
 
     [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float jumpForce = 4f;
+    public bool isJumping = false;
     
 
     private float moveInput;
     private float lastMoveInput = 1f;
-    private bool isFacingRight = true;
+    private bool isFacingRight = false;
+  
+    
+
 
     private void Awake()
     {
@@ -23,6 +28,14 @@ public class PlayerController : MonoBehaviour
         moveInput = Input.GetAxis("Horizontal");
         if(moveInput != 0f)
             lastMoveInput = moveInput;
+
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = true;
+        }
+
+        Flip();
     }
 
     private void FixedUpdate()
@@ -38,5 +51,22 @@ public class PlayerController : MonoBehaviour
     void Jump()
     {
 
+    }
+    
+    void Flip()
+    {
+        if (isFacingRight && moveInput < 0f || !isFacingRight || moveInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x = 1f; 
+            transform.localScale = ls;
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false; 
     }
 }
