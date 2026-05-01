d9180667feed89a29319be6de3e9f8e3b567130b
Furizax <148867645+Furizax@users.noreply.github.com>
2026-05-01 13:09:38 -0400

    Patrol Prototype

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
     public Transform player;
+    public GameObject pointA; 
+    public GameObject pointB;
 
+    private Rigidbody2D rb;
+    private Transform currentTarget;
     EnnemyStat stats;
-
     private float distanceToPlayer;
+    private bool isPlayerDetected = false;
+
 
     enum State
     {
@@ -22,20 +28,46 @@ public class EnemyAI : MonoBehaviour
     private void Start()
     {
         stats = GetComponent<EnnemyStat>();
+        rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        currentTarget = pointB.transform;
     }
 
     private void Update()
     {
+        Vector2 Point = currentTarget.position - transform.position;
+        if(currentTarget == pointB.transform)
+        {
+            rb.velocity = new Vector2(stats.moveSpeed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-stats.moveSpeed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currentTarget.position) < 0.5f && currentTarget == pointB.transform)
+        {
+            currentTarget = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentTarget.position) < 0.5f && currentTarget == pointA.transform)
+        {
+            currentTarget = pointB.transform;
+        }
+
+        /*
         distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, stats.moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, stats.moveSpeed * Time.deltaTime); 
+        */
+
+
     }
 
     void UpdateState()
     {
-
+        
     }
 
     void HandleState()
@@ -45,12 +77,12 @@ public class EnemyAI : MonoBehaviour
 
     void HandleIdle()
     {
-        // rien ou patrouille simple
+
     }
 
     void HandleChase()
     {
-        // se déplacer vers joueur
+
     }
 
     void HandleAttack()
